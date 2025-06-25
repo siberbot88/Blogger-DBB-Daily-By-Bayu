4a3435ec06e4cd2d2cc9bab703c84d8dcd683ab0
Mohammad Bayu Rizki <widjanarko020@gmail.com>
2025-06-26 00:52:15 +0700

    Implement login component with form handling and styling for admin access

--- client/src/components/admin/Login.jsx
+++ client/src/components/admin/Login.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 
 const Login = () => {
+
+    const handleSubmit = async (e)=> {
+        e.preventDefault()
+    }
+
   return (
-    <div>
-      <div></div>
+    <div className='flex items-center justify-center h-screen'>
+      <div className='w-full max-w-sm p-6 max-md:m-6 border border-primary/30 shadow-xl shadow-primary/15 rounded-lg'>
+        <div className='flex flex-col items-center justify-center'>
+            <div className='w-full py-6 text-center'>
+                <h1 className='text-3xl font-bold'><span className='text-primary'>Admin</span> Login</h1>
+                <p className='font-light'>Enter your credentials to access the admin panel</p>
+            </div>
+            <form onSubmit={handleSubmit}>
+                <div className='flex flex-col'>
+                    <label htmlFor="">Email</label>
+                    <input type="email" required placeholder='your email id' className='border-b-2 border-gray-300 p-2 outline-none mb-6' />
+                </div>
+            </form>
+        </div>
+      </div>
     </div>
   )
 }
