060d367ac4f0ceda14ebe1f0b0eb4e80dc368b90
Mohammad Bayu Rizki <widjanarko020@gmail.com>
2025-07-05 15:53:19 +0700

    Refactor admin routing to always render Layout component and enhance Login component with state management for email and password

--- client/src/components/admin/Login.jsx
+++ client/src/components/admin/Login.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
 
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     const handleSubmit = async (e)=> {
         e.preventDefault()
     }
@@ -18,12 +20,14 @@ const Login = () => {
                 {/* Email */}
                 <div className='flex flex-col'>
                     <label htmlFor="">Email</label>
-                    <input type="email" required placeholder='your email id' className='border-b-2 border-gray-300 p-2 outline-none mb-6' />
+                    <input onChange={e=> setEmail(e.target.value)} value={email}
+                    type="email" required placeholder='your email id' className='border-b-2 border-gray-300 p-2 outline-none mb-6' />
                 </div>
                 {/* Password */}
                 <div className='flex flex-col'>
                     <label htmlFor="">Password</label>
-                    <input type="password" required placeholder='your password' className='border-b-2 border-gray-300 p-2 outline-none mb-6' />
+                    <input onChange={e=> setPassword(e.target.value)} value={password}
+                    type="password" required placeholder='your password' className='border-b-2 border-gray-300 p-2 outline-none mb-6' />
                 </div>
                 <button type='submit' className='w-full py-3 font-medium bg-primary text-white rounded cursor-pointer hover:bg-primary/90 transition-all '>Login</button>
             </form>
--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { assets } from '../../assets/assets'
 
 const Dashboard = () => {
+
+    const [dashboardData, setDashboardData] = useState({
+      blogs: 0,
+      comments: 0,
+      draft:0,
+      recentBlogs: []
+    })
+
+    const fetchDashboard = async () => {
+      setDashboardData(dashboardData)
+    }
+
+    useEffect(()=> {
+      fetchDashboard()
+    },[])
+
   return (
-    <div>
+    <div className='flex-1 p-4 md:p-10 bg-blue-50/50'>
       
+      <div className='flex flex-wrap gap-4'>
+        <div>
+          <img src={assets.dashboard_icon_1} alt="" />
+          <div className='flex items-center gap-4 bg-white p-4 min-w-58 rounded-shadow cursor-pointer hover-scale-105 transition-all'>
+            <p className='text-xl font-semibold test-gray-600'>{dashboardData.blogs}</p>
+            <p className='text-gray-400 font-light'>Blogs</p>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
